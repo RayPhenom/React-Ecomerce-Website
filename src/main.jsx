f5dd441270997dbdb738da83bc6f39a63ce44189
RayPhenom <lumuonix@gmail.com>
2024-07-26 13:34:44 +0300

    update on navitems

--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,37 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import "swiper/css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
-import "././assets/css/icofont.min.css";
-import "././assets/css/animate.css";
-import "././assets/css/style.min.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import 'swiper/css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import '././assets/css/icofont.min.css';
+import '././assets/css/animate.css';
+import '././assets/css/style.min.css';
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./home/Home.jsx";
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './home/Home.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
-    children: [
-      { path: "/", element: <Home /> },
-      {
-        path: "/blog",
-        element: <Blog />,
+    element: <App/>,
+    children:[
+      {path:"/",
+        element:<Home/>
       },
-    ],
+      {path:"/blog",
+        element:<blog/>
+      }
+
+    ]
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />
-);
+)
