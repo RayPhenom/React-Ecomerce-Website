211aea0c39d9b325d3aeaf0684d297a5eba36369
RayPhenom <lumuonix@gmail.com>
2024-07-26 14:58:30 +0300

    Works on filter option

--- src/home/Banner.jsx
+++ src/home/Banner.jsx
@@ -1,4 +1,6 @@
+import { useState } from 'react'
 import React from 'react'
+import productData  from '../products.json'
 
 
 const title = (
@@ -23,11 +25,33 @@ const bannerList = [
     ];
 
 const Banner = () => {
+    const [searchInput, setSearchInput ] = useState("");
+    const [filteredProducts, setfilteredProducts ] = useState("productData");
+    //console.log(productData);
+    
+    const handleSearch = e => {
+        console.log(e.target.value);
+        const searchTerm = e.target.value;
+        setSearchInput(searchTerm);
+    }
+
+    const filtered = productData.filter((product) =>
+    product.name.toLowerCase().includes(searchInput.toLowerCase())
+    );
+    setfilteredProducts(filtered);
+
   return (
     <div className='banner-section style-4'>
         <div className='container'>
             <div className='banner-content'>
                 {title}
+                <form >
+                    <input type="text" name='search' id="search" placeholder='Search your product' value={searchInput} onChange={handleSearch} />
+                </form>
+                <p>{desc}</p>
+                <ul className='lab-ul'>
+
+                </ul>
 
             </div>
             </div> 
