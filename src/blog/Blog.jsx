81d78353548283ee90150b9e7a1b5e881d15d54d
RayPhenom <lumuonix@gmail.com>
2024-08-22 14:47:40 +0300

    Update on Blog but not done

--- src/blog/Blog.jsx
+++ src/blog/Blog.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
-import PageHeader from '../components/PageHeader'
+import React from 'react';
+import PageHeader from '../components/PageHeader';
+import blogList from '../utilis/blogdata';
 
-const blog = () => {
-      
+const Blog = () => {
   return (
     <div>
-      <PageHeader title="Our Blog page" curPage="Blogs"/>
-
+      
+      {/* Blog List */} 
+      <div className="blog-list, display-flex, flex-wrap:wrap, justify-content: space-between, gap-20px ">
+        {blogList.map((blog) => (
+          <div key={blog.id} className="blog-item, box-sizzing: border-box, padding-15px, border-radius:8px ">
+            <img src={blog.imgUrl} alt={blog.imgAlt} />
+            <h3>{blog.title}</h3>
+            <p>{blog.desc}</p>
+            <div className="blog-meta">
+              {blog.metaList.map((meta, index) => (
+                <span key={index}>
+                  <i className={meta.iconName}></i> {meta.text}
+                </span>
+              ))}
+            </div>
+            <div className="blog-footer">
+              <span>{blog.commentCount} Comments</span>
+              <button>{blog.btnText}</button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default blog
+export default Blog;
